a93dd5abf2783efc7d9da4aaf48e1d3155cfb362
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-02-10 13:45:27 +0200

    I added mainday

---
+++ mainday2.py
@@ -0,0 +1,26 @@
+ #Data Types
+
+# String
+# Interger
+# Boolean
+# Float
+
+# # print("Hello"[4])
+
+# print(123 * 456)
+
+# Bmi Calculator
+
+# height = input("Please enter your height in m: ")
+
+# weight = input("Please input weight in KG: ")
+
+# weightint = int(weight)
+
+# heightflt = float(height)
+
+# bmi = (weightint) / (heightflt)**2
+
+# print(str((bmi)))
+
+age = input()
